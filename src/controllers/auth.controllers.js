6c961fc78db2732994f65e006351da2ba6a615af
leticia-chijo <leticia.chijo@gmail.com>
2023-05-24 19:40:08 -0300

    Requisito: POST /signup

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,21 @@
+
+import bcrypt from "bcrypt"
+import { createUserDB, getUserByEmailDB } from "../repositories/user.repository.js"
+
 export async function signUp(req, res) {
-    res.send("signup")
+    const { name, email, password } = req.body
+
+    try {
+        const user = await getUserByEmailDB(email)
+        if (user.rowCount !== 0) return res.status(409).send({ message: "E-mail já foi cadastrado!" })
+
+        const hash = bcrypt.hashSync(password, 10)
+        await createUserDB(name, email, hash)
+
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 
 export async function signIn(req, res) {
