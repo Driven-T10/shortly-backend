3880c2e4622d3d4e57884d6deca641e6851c76c9
leticia-chijo <leticia.chijo@gmail.com>
2023-05-24 20:14:12 -0300

    Requisito: POST /urls/shorten

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -1,5 +1,19 @@
+import { customAlphabet } from 'nanoid'
+import { createShortUrlDB } from '../repositories/url.repository.js'
+const nanoid = customAlphabet('1234567890abcdef', 8)
+
 export async function shortenUrl(req, res) {
-    res.send("shortenUrl")
+    const { url } = req.body
+    const { userId } = res.locals
+    const shortUrl = nanoid()
+
+    try {
+        const {rows: [result]} = await createShortUrlDB(url, shortUrl, userId)
+        res.status(201).send(result)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
 }
 
 export async function getUrl(req, res) {
