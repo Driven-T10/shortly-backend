7d0f536f2198e7c1cd65a014ee06dc4f674ed6bb
leticia-chijo <leticia.chijo@gmail.com>
2023-05-24 21:21:22 -0300

    Requisito: GET /users/me

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,8 +1,23 @@
+import { getUrlsByUserDB, getUserByIdDB } from "../repositories/user.repository.js"
+
 export async function getCurrentUser(req, res) {
-    res.send("getCurrentUser")
+    const { userId } = res.locals
+
+    try {
+        const { rows: [user] } = await getUserByIdDB(userId)
+        const { rows: urls } = await getUrlsByUserDB(userId)
+        res.send({ ...user, shortenedUrls: [...urls] })
+    } catch (err) {
+        res.stats(500).send(err.message)
+    }
 }
 
 
 export async function getUserRanking(req, res) {
-    res.send("getUserRanking")
+
+    try {
+        res.send("getUserRanking")
+    } catch (err) {
+        res.stats(500).send(err.message)
+    }
 }
